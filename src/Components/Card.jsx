03b41bdd8ec4e27a1d64ec580fb9dd94701c49a2
Himanshu <himanshujangid364@gmail.com>
2024-03-11 23:36:18 +0530

    Handling Events Done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,43 +1,22 @@
-import React from 'react'   
+import React from 'react' ;  
 
 function Card(){
+
     const data = [
-        {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww' ,
-        name:"Amazon Basics",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas.",
-        instock:true},
-
-        {image: 'https://images.unsplash.com/photo-1612705166546-641e59cef326?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZ2F0ZXxlbnwwfHwwfHx8MA%3D%3D' ,
-        name:"Daily Basics",
-        description: "Lorem, ipsum doluptas.",
-        instock: false},
-
-        {image: 'https://images.unsplash.com/photo-1552858725-2758b5fb1286?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVkfGVufDB8fDB8fHww' ,
-        name:"Weekend Basics",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas . Ea, perferendis nem.",
-        instock: false}
-
-        ]
-
+        {name: "Tum Se Hi", description:"lorem ipsum dolor sit amet consectetur consectetur adipiscing elit esse cillum dolore magna"},
+        {name: "Mera Mann", description:"lorem ipsum dolor sit amet consect et dolor consectetur adipiscing elit esse cillum dolore magna"}
+    ]
+    const onClickFtn = () => {alert("download hora h")};
     return (
-        <div className='w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
-            {data.map((elem,index)=>(
-                <div key={index} className='w-52 bg-zinc-100 rounded-md overflow-hidden'>
-                <div className='w-full h-32 bg-zinc-300 '>
-                    <img className='w-full h-full object-cover' src={elem.image} alt="" />
+        <div className='w-full h-screen bg-zinc-300 flex flex-col gap-10 justify-center items-center'>
+            {data.map((item,index) => (
+                <div className='w-60 px-3 py-2 bg-zinc-100 rounded-md' key={index}>
+                <h3 className='font-semibold text-xl'>{item.name}</h3>
+                <p className='text-xs mt-2'>{item.description}</p>
+                <button onClick={onClickFtn} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 mt-2'>Download Now</button>
                 </div>
-                <div className='w-full px-3 py-4 '>
-                    <h2 className='font-semibold'>{elem.name}</h2>
-                    <p className='text-xs mt -3'>{elem.description}</p>
-                    <button className={`px-4 py-1 ${elem.instock ? 'bg-sky-600' : 'bg-red-600'} text-xs rounded text-zinc-100 mt-3`} >
-                        {elem.instock ? "In Stock" : "Out Of Stock"}
-                    </button>
-                </div>
-            </div>
             ))}
         </div>
     )
 }
-
 export default Card;
-//ese hum reusable components bnate h
