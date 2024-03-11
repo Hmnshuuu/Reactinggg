2d435204c33babf4a55e41973b12a352604a5476
Himanshu <himanshujangid364@gmail.com>
2024-03-11 22:54:31 +0530

    Conditional Rendering Done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function Card(){
     const data = [
-        {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww' ,name:"Amazon Basics",description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas."},
-        {image: 'https://images.unsplash.com/photo-1612705166546-641e59cef326?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZ2F0ZXxlbnwwfHwwfHx8MA%3D%3D' ,name:"Daily Basics",description: "Lorem, ipsum doluptas."},
-        {image: 'https://images.unsplash.com/photo-1552858725-2758b5fb1286?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVkfGVufDB8fDB8fHww' ,name:"Weekend Basics",description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas . Ea, perferendis nem."}
+        {image: 'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww' ,
+        name:"Amazon Basics",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas.",
+        instock:true},
+
+        {image: 'https://images.unsplash.com/photo-1612705166546-641e59cef326?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZ2F0ZXxlbnwwfHwwfHx8MA%3D%3D' ,
+        name:"Daily Basics",
+        description: "Lorem, ipsum doluptas.",
+        instock: false},
+
+        {image: 'https://images.unsplash.com/photo-1552858725-2758b5fb1286?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVkfGVufDB8fDB8fHww' ,
+        name:"Weekend Basics",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, perferendis nemo? Voluptas . Ea, perferendis nem.",
+        instock: false}
 
         ]
 
@@ -18,6 +29,9 @@ function Card(){
                 <div className='w-full px-3 py-4 '>
                     <h2 className='font-semibold'>{elem.name}</h2>
                     <p className='text-xs mt -3'>{elem.description}</p>
+                    <button className={`px-4 py-1 ${elem.instock ? 'bg-sky-600' : 'bg-red-600'} text-xs rounded text-zinc-100 mt-3`} >
+                        {elem.instock ? "In Stock" : "Out Of Stock"}
+                    </button>
                 </div>
             </div>
             ))}
