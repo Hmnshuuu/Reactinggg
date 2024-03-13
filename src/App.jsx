dc5bd8735980640190cc18e2ff8b74730dbdc5f4
Himanshu <himanshujangid364@gmail.com>
2024-03-14 00:03:48 +0530

    Manage UseState Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Card from "./Components/Card";
+import React,{useState} from 'react';
+// import Card from "./Components/Card";
 
 function App(){
+  const [banned,setBanned] = useState(false);
   return(
-    <>
-      <Card />
-      <h1>hey</h1>
-    </>
+    <div className='p-4'>
+      <h1>{banned.toString()}</h1>
+      <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-lg bg-blue-500'>BAN KRO</button>
+    </div>
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,22 @@
-import React from 'react' ;  
+// import React from 'react' ;  
 
-function Card(){
+// function Card(){
 
-    const data = [
-        {name: "Tum Se Hi", description:"lorem ipsum dolor sit amet consectetur consectetur adipiscing elit esse cillum dolore magna"},
-        {name: "Mera Mann", description:"lorem ipsum dolor sit amet consect et dolor consectetur adipiscing elit esse cillum dolore magna"}
-    ]
-    const onClickFtn = () => {alert("download hora h")};
-    return (
-        <div className='w-full h-screen bg-zinc-300 flex flex-col gap-10 justify-center items-center'>
-            {data.map((item,index) => (
-                <div className='w-60 px-3 py-2 bg-zinc-100 rounded-md' key={index}>
-                <h3 className='font-semibold text-xl'>{item.name}</h3>
-                <p className='text-xs mt-2'>{item.description}</p>
-                <button onClick={onClickFtn} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 mt-2'>Download Now</button>
-                </div>
-            ))}
-        </div>
-    )
-}
-export default Card;
+//     const data = [
+//         {name: "Tum Se Hi", description:"lorem ipsum dolor sit amet consectetur consectetur adipiscing elit esse cillum dolore magna"},
+//         {name: "Mera Mann", description:"lorem ipsum dolor sit amet consect et dolor consectetur adipiscing elit esse cillum dolore magna"}
+//     ]
+//     const onClickFtn = () => {alert("download hora h")};
+//     return (
+//         <div className='w-full h-screen bg-zinc-300 flex flex-col gap-10 justify-center items-center'>
+//             {data.map((item,index) => (
+//                 <div className='w-60 px-3 py-2 bg-zinc-100 rounded-md' key={index}>
+//                 <h3 className='font-semibold text-xl'>{item.name}</h3>
+//                 <p className='text-xs mt-2'>{item.description}</p>
+//                 <button onClick={onClickFtn} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 mt-2'>Download Now</button>
+//                 </div>
+//             ))}
+//         </div>
+//     )
+// }
+// export default Card;
