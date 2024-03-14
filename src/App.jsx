1d01859aebbcbcfaca1702b0febd68daada404a2
Himanshu <himanshujangid364@gmail.com>
2024-03-14 22:34:51 +0530

    useState Advanced Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 // import Card from "./Components/Card";
 
-function App(){
-  const [banned,setBanned] = useState(false);
-  return(
+function App() {
+  const [val, setVal] = useState({ name: "harsh", isBanned: false });
+  return (
     <div className='p-4'>
-      <h1>{banned.toString()}</h1>
-      <button onClick={()=>setBanned(!banned)} className='px-2 py-1 mt-2 rounded-full text-lg bg-blue-500'>BAN KRO</button>
+      <h1>name: {val.name}</h1>
+      <h2>banned : {val.isBanned.toString()}</h2>
+      <button onClick={() => setVal({ ...val, isBanned: !val.isBanned })} className={`px-3 py-1 mt-2 rounded-full text-white text-xs ${val.isBanned ? "bg-blue-500" : "bg-red-500"}`}>BAN KRO</button>
     </div>
   )
 }
